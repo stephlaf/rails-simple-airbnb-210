ff70a7a608aa7dcb41268e0ce5ce14397670eafd
Stephane Lafontaine <stephane@medqigong.ca>
2019-02-14 17:59:33 -0500

    Added all views and links, navbar. Working on cards design

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Clearing db..."
+Flat.destroy_all
+puts "Seeding.."
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '101 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  photo: 'https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Paris',
+  address: '102 Clifton Gardens London W9 1DT',
+  description: 'Blablabla',
+  price_per_night: 80,
+  number_of_guests: 4,
+  photo: 'https://images.unsplash.com/photo-1513584684374-8bab748fbf90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=746&q=80'
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Berlin',
+  address: '103 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 85,
+  number_of_guests: 2,
+  photo: 'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=753&q=80'
+)
+Flat.create!(
+  name: 'Light & Spacious Flat Longueuil',
+  address: '104 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 95,
+  number_of_guests: 1,
+  photo: 'https://images.unsplash.com/photo-1518780664697-55e3ad937233?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=401&q=80'
+)
+puts "Done!"
